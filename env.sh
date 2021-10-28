44695bd9a07d201c5dc4665299a49e5167c5743c
August <mingxin.android@gmail.com>
2021-10-28 13:04:05 +0800

    upload android part1

---
+++ env.sh
@@ -0,0 +1,1 @@
+export PATH=/opt/gcc-linaro-aarch64-none-elf-4.8-2013.11_linux/bin:/opt/gcc-linaro-arm-none-eabi-4.8-2014.04_linux/bin:/opt/gcc-linaro-6.3.1-2017.02-x86_64_aarch64-linux-gnu/bin:/opt/gcc-linaro-6.3.1-2017.02-x86_64_arm-linux-gnueabihf/bin:$PATH
